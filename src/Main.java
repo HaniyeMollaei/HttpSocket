220d6678e49a264a2344861e5fac6097b0049a23
haniye mollaei <haniyemolaei1378@gmail.com>
2020-10-19 21:24:14 +0330

    completed code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.Set;
@@ -8,39 +6,37 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    final static int port = 80 ;
+    final static int port = 80;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         var studentId = "";
         var input = "";
-        var methods = Set.of("GET" , "POST" , "PATCH" , "PUT" , "DELETE");
-        System.out.println("Welcome To HTTP Project");
-
-       // System.out.println("Pleas enter IP : ");
+        var methods = Set.of("GET", "POST", "PATCH", "PUT", "DELETE");
         var pattern = Pattern.compile("(?i)(http://)?([-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b)([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)");
 
-        while (true){
+        while (true) {
             System.out.println("Pleas enter URL or command : ");
             input = sc.nextLine();
             input = input.trim().toLowerCase();
 
-            if (input.equals("exit")){
+            if (input.equals("exit")) {
                 break;
             }
-            if(input.equals("set-student-id-header")){
+            if (input.equals("set-student-id-header")) {
                 System.out.println("enter student id: ");
-                input=sc.nextLine();
-                input=input.trim().toLowerCase();
+                input = sc.nextLine();
+                input = input.trim().toLowerCase();
                 studentId = input;
                 continue;
             }
-            if (input.equals("remove-student-id-header")){
+            if (input.equals("remove-student-id-header")) {
                 studentId = "";
                 continue;
             }
+
             var tmp_url = pattern.matcher(input);
-            if (tmp_url.matches()){
+            if (tmp_url.matches()) {
                 System.out.println("Enter http method : ");
                 System.out.println(
                         "GET for get method,\n" +
@@ -49,35 +45,28 @@ public class Main {
                                 "PATCH for patch method, \n" +
                                 "DELETE for delete method, \n");
 
-                var method= sc.nextLine().trim().toUpperCase();
+                var method = sc.nextLine().trim().toUpperCase();
 
-                if (!methods.contains(method)){
+                if (!methods.contains(method)) {
                     System.out.println("bad method");
                     continue;
                 }
-                System.out.println("matches: ");
-                var hostname =tmp_url.group(2);
-                var resorse =tmp_url.group(3);
-                if (resorse.isEmpty()){
-                    resorse="/";
+
+                var hostname = tmp_url.group(2);
+                var recource = tmp_url.group(3);
+                if (recource.isEmpty()) {
+                    recource = "/";
                 }
 
                 try {
-                    var socket = new Socket(hostname,port);
+                    var socket = new Socket(hostname, port);
                     var output = new OutputStreamWriter(socket.getOutputStream());
-                    var inpStream = socket.getInputStream();
 
-                    output.write(makeRequestBody(method,resorse,studentId));
+                    output.write(makeRequestBody(method, recource, studentId));
                     output.flush();
-                    var inSc= new Scanner(socket.getInputStream());
-                    var inputSb=new StringBuilder();
-                    while (inSc.hasNextLine()){
-                        inputSb.append(inSc.nextLine()).append("\n");
-                    }
 
+                    parsInput(socket.getInputStream());
                     socket.close();
-
-                    System.out.println(inputSb.toString());
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -87,12 +76,12 @@ public class Main {
     }
 
     private static String makeRequestBody(String method, String resource, String studentId) {
-        String crlf="\r\n";
+        String crlf = "\r\n";
         var sb = new StringBuilder();
         sb.append(method).append(" ").append(resource).append(" ").append("HTTP/1.0").append(crlf);
         sb.append("Accept: */*").append(crlf);
         sb.append("Accept-Encoding: gzip, deflate, br").append(crlf);
-        if (!studentId.isEmpty()){
+        if (!studentId.isEmpty()) {
             sb.append("x-student-id: ").append(studentId).append(crlf);
         }
         sb.append(crlf);
@@ -100,49 +89,84 @@ public class Main {
         return sb.toString();
     }
 
-    private static void parsInput(InputStream inputStream){
+    private static void parsInput(InputStream inputStream) {
 
-        var headerScanner = new Scanner(inputStream);
+        FileWriter writer ;
+        var headerSc = new Scanner(inputStream);
+        var response = new StringBuilder();
 
-        var header = new StringBuilder();
+        var tmp_str = "";
+        var type = "";
+        var error_num = "";
 
-        var inp = "#";
-        var charset= "utf-8";
-        var reachedBody=false;
-        var isHtml= false;
-
-        var body = new StringBuilder();
-
-        while (headerScanner.hasNextLine()){
-            System.out.println("here");
-            inp=headerScanner.nextLine().toLowerCase();
-            if (inp.isEmpty()){
-                reachedBody=true;
-            }
-
-            if (inp.contains("content-type")){
-                if (inp.contains("text/html")){
-                    isHtml=true;
+        var first_line = headerSc.nextLine();
+        //identify type of response and write it in CLI
+        if (first_line.equals("HTTP/1.1 200 OK") || first_line.equals("HTTP/1.0 200 OK")) {
+            while (headerSc.hasNextLine()) {
+                tmp_str = headerSc.nextLine();
+                if (tmp_str.toLowerCase().contains("content-type")) {
+                    type = tmp_str.split("/")[1].split(";")[0];
                 }
+                response.append(tmp_str).append("\n");
             }
+        } else {              //Error
+            type = "error";
+            while (headerSc.hasNextLine()) {
+                tmp_str = headerSc.nextLine();
+                response.append(tmp_str).append("\n");
+            }
+        }
 
-            if (!reachedBody) {
-                header.append(inp).append("\n");
-            }else {
-                body.append(inp).append("\n");
-            }
+        switch (type) {
+
+            case "json":
+
+                try {
+                    writer = new FileWriter("test.json");
+                    writer.write(response.toString().split("Connection: close\n\n")[1]);
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+                System.out.println("*** Json file saved on your pc");
+                break;
+
+
+            case "html":
+
+                try {
+                    writer = new FileWriter("test.html");
+                    writer.write(response.toString().split("Connection: close\n")[1]);
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+                System.out.println("*** Html file saved on your pc");
+                break;
+            case "plain":
+
+                System.out.println(response.toString().split("Connection: close\n")[1]);
+                break;
+            default:    //error
+                System.out.println("*** Error ***");
+                error_num = first_line.split(" ")[1];
+                System.out.println("*** Error number : " + error_num);
+                System.out.print("*** Error description : ");
+                for (int i = 2; i < first_line.split(" ").length; i++) {
+                    System.out.print(first_line.split(" ")[i] + " ");
+
+                }
+                System.out.println();
+                break;
 
         }
 
-        if (isHtml){
-            // Files.writeString(Path.of("./taha.html") , body.toString());
-        }
         System.out.println("****** Server response ******");
-        System.out.println(header.toString());
-        System.out.println(body.toString());
+        System.out.println(response.toString());
         System.out.println("****** End of server response ******");
-
-
     }
 }
 
